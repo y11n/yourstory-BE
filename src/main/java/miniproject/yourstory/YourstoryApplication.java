0fc44de669fa4d6c1f65958d3ac6ce5ca7a9312a
y11n <samtori3849@gmail.com>
2024-11-18 14:09:39 +0900

    초기 세팅

---
+++ src/main/java/miniproject/yourstory/YourstoryApplication.java
@@ -0,0 +1,13 @@
+package miniproject.yourstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourstoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miniproject/yourstory/YourstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.yourstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourstoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
