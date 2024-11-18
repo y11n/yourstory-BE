008de9f8ee12824d9547a305eaff8a58555d56a7
y11n <samtori3849@gmail.com>
2024-11-18 21:44:29 +0900

    [Fix] Cors 설정

--- src/main/java/miniproject/yourstory/config/SecurityConfig.java
+++ src/main/java/miniproject/yourstory/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package miniproject.yourstory.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import miniproject.yourstory.config.filter.JWTFilter;
 import miniproject.yourstory.config.filter.LoginFilter;
 import miniproject.yourstory.config.jwt.JWTUtil;
@@ -13,6 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +43,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((cors -> cors.configurationSource(new CorsConfigurationSource(){
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         // csrf disable
         http
