0fcfff4f119f4ccbfa7d943cd2d17d1112cde6a7
y11n <samtori3849@gmail.com>
2025-01-24 21:47:38 +0900

    [Fix] 봉사 목록 조회 응답 데이터 수정
    봉사 목록 응답 데이터에서 장소, 담당자, 내용, 인원 수 삭제

--- src/main/java/miniproject/yourstory/dto/WorkListDTO.java
+++ src/main/java/miniproject/yourstory/dto/WorkListDTO.java
@@ -1,2 +1,22 @@
-package miniproject.yourstory.dto;public class WorkListDto {
+package miniproject.yourstory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import miniproject.yourstory.entity.Period;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkListDTO {
+
+    private long id;
+    private String title;
+    private String state;
+    private Period recruitment;
+    private int period;
+    private String org;
+    private String day;
 }
